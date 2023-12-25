6a8c324da1626ecbb3f854612b951189d95fb7e0
Pluto-alpha <ravialphonic@gmail.com>
2023-12-25 15:50:03 +0530

    --update--

--- server/config/generatePdf.js
+++ server/config/generatePdf.js
@@ -12,7 +12,8 @@ const generatePdf = async (visitor, passMaker) => {
         const templateContent = fs.readFileSync(templatePath, 'utf8');
         const template = hbs.compile(templateContent);
         const validOn = moment(visitor.validOn).format("DD MMM YYYY, hh:mm A");
-        const validUpTo = moment(visitor.validUpTo).format("DD MMM YYYY, hh:mm A");
+        const validUpTo = moment(visitor.validUpTo).format("DD MMM YYYY");
+        const time = moment(visitor.validUpTo).format("hh:mm A");
         const html = template({
             name: visitor.visitorName,
             passNo: visitor.passNo,
@@ -23,11 +24,15 @@ const generatePdf = async (visitor, passMaker) => {
             idProofNo: visitor.idProofNo,
             validOn: validOn,
             validUpTo: validUpTo,
+            time:time,
             passCreater:passMaker,
         });
         await page.setContent(html);
-        const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
-        await browser.close();
+        const pdfBuffer = await page.pdf({ 
+            margin: { top: '30px', right: '30px', bottom: '30px', left: '30px' },
+            format: 'A4', 
+            printBackground: true 
+        });
         return pdfBuffer;
     } catch (error) {
         console.error('Error generating PDF:', error);
